5fe4497b5a6d377bcc7f2fb3852178309f5857b8
Brian Leonard <brianleonard@Brians-MacBook-Pro.local>
2024-05-28 10:52:37 -0400

    update psychDSfile interface to include fileText, issueInfo, and expanded

--- src/types/file.ts
+++ src/types/file.ts
@@ -1,3 +1,11 @@
+export interface issueInfo {
+  key: string
+  severity?: string
+  reason?: string
+  evidence?: string
+}
+
+
 export interface psychDSFile {
     // Filename
     name: string
@@ -9,6 +17,12 @@ export interface psychDSFile {
     ignored: boolean
     // ReadableStream to file raw contents
     stream: ReadableStream<Uint8Array>
+    // string storage of file contents
+    fileText: string
+    // object for temporarily storing issues with jsonld before issue object is created in validate()
+    issueInfo: issueInfo[]
+    // slot to hold expanded version of jsonld
+    expanded: object
     // Resolve stream to decoded utf-8 text
     text: () => Promise<string>
     // Read a range of bytes
